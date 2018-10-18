b38903375b7de19a5a2e293a91343d76056ed94e
Christina <christina.golovach@gmail.com>
2018-10-18 20:35:33 +0300

    Add core logic for transform double to word

--- TransformLogic/Transform.cs
+++ TransformLogic/Transform.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
 
 namespace TransformLogic
 {
-    public class Class1
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Transform
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static string[] TransformToWords(double[] numbers)
+        {
+            int i = 0;
+            string[] numbersInWordsView = new string[numbers.Length];
+            
+            foreach(double number in numbers)
+            {
+                numbersInWordsView[i] = TransformDoubleToWord(number);
+                i++;
+            }
+
+            return numbersInWordsView;
+        }
+
+        private static string TransformDoubleToWord(double number)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-Us");
+
+            char[] symbolsOfNumber = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', '.', '-', '+' };
+            string[] wordsOfSymbols = new string[] { "zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "E ", "point ", "minus ", "plus "};
+            string numberInStringView = number.ToString();
+            StringBuilder numberInWordView = new StringBuilder();
+
+            foreach(char symbol in numberInStringView)
+            {
+                int index = Array.IndexOf(symbolsOfNumber, symbol);
+                numberInWordView.Append(wordsOfSymbols[index]);
+            }
+
+            numberInWordView.Remove(numberInWordView.Length-1, 1);
+            return numberInWordView.ToString();
+        }
     }
 }
